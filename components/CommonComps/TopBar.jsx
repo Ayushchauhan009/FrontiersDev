3a4f6130e68d5aab164510d3c7d42c9cf2d74f81
aayushchauhan10 <aayushchauhanrajput47@gmail.com>
2024-01-28 11:27:58 +0530

    Fix Navbar Logo

--- components/CommonComps/TopBar.jsx
+++ components/CommonComps/TopBar.jsx
@@ -4,9 +4,9 @@ import Navbar from "./Navbar";
 export default function Topbar({ trigger, nav, setNav }) {
   return (
     <div className="bg-background-primary-dark-color fixed z-50 top-0 w-full  border-txt-primary-dark-bg/10  border-solid h-16 mx-auto topbar-section">
-      <div className="mx-auto flex justify-between normal:px-6 items-center pt-8 h-full max-w-7xl tiny:px-4 lg:px-32">
+      <div className="mx-auto flex justify-between md:px-6 items-center pt-12 h-full max-w-7xl px-4 lg:px-10">
         <div className="font-accentFont text-txt-primary-dark-bg font-black text-sm block lg:text-lg">
-          <img src="/logoNav.png" alt="" className="w-60 h-auto" />
+          <img src="/logoNav.png" alt="" className="w-16 h-auto" />
         </div>
 
         <div className="relative h-full flex justify-around items-center w-auto">
