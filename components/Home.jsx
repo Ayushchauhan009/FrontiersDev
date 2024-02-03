e035f09d9afd52ad493d222328e2f8eddd152ad3
aayushchauhan10 <project@melangedigital.in>
2024-02-03 18:40:03 +0530

    Enhancing Service Component

--- components/Home.jsx
+++ components/Home.jsx
@@ -46,7 +46,7 @@ const Header = ({ trigger, nav, setNav }) => {
       <TopBar trigger={trigger} nav={nav} setNav={setNav} />
       <div className="header text-white">
         <div className="visibleImage">
-          <p className="font-bold flex flex-col text-[30px] px-6 lg:px-0 lg:text-[70px] font-exo text-center  justify-center items-center h-full lg:h-screen">
+          <p className="font-bold flex flex-col text-[30px] px-6 lg:px-0 lg:text-[70px] font-clash text-center  justify-center items-center h-full lg:h-screen">
             Elevate Your Web Presence <br className="hidden lg:block" /> with
             Innovation{" "}
             <span className="mt-20 shadow-2xl animate-bounce">
@@ -64,7 +64,7 @@ const Header = ({ trigger, nav, setNav }) => {
             clipPath: calculateClipPath(),
           }}
         >
-          <p className="font-bold text-[30px] flex-col px-6 lg:px-0 lg:text-[70px] font-exo text-center flex justify-center items-center h-full lg:h-screen">
+          <p className="font-bold text-[30px] flex-col px-6 lg:px-0 lg:text-[70px]  text-center flex justify-center items-center h-full lg:h-screen">
             Boost Your Digital Impact <br className="hidden lg:block" /> with
             Innovation{" "}
             <span className="mt-20 shadow-2xl animate-bounce">
--- components/Services.jsx
+++ components/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
   const [visibleIndex, setVisibleIndex] = useState(0);
   const [isSticky, setIsSticky] = useState(false);
 
-  const images = ["/1.jpg", "/2.jpg", "/3.jpg", "/4.jpg", "/5.jpg"];
+  const images = ["/1stService.png", "/2.jpg", "/3.jpg", "/4.jpg", "/5.jpg"];
 
   useEffect(() => {
     const handleScroll = () => {
@@ -32,15 +32,15 @@ const Services = () => {
   }, [isSticky]);
 
   return (
-    <div className="relative bg-[#a2f2ff] pb-20 text-black">
+    <div className="relative bgOpacity2 pb-20 text-white">
       <div
         className={` px-5 lg:px-24 max-container ${
-          isSticky ? "sticky top-20" : ""
+          isSticky ? "sticky inset-0 top-20" : ""
         }`}
       >
         <div className="flex flex-col lg:flex-row items-start lg:space-x-20 justify-between lg:pt-20 ">
-          <div className="font-bold w-full lg:w-[150%] sticky top-10 lg:top-20 font-exo">
-            <p className="my-10 font-semibold text-3xl lg:text-5xl font-serif ">
+          <div className="font-bold w-full lg:w-[100%] sticky top-10 lg:top-20 font-exo">
+            <p className="mt-10 mb-20 font-semibold  text-3xl lg:text-[55px] headings ">
               Services
             </p>
             <div className="relative" style={{ height: "300px" }}>
@@ -49,89 +49,57 @@ const Services = () => {
                   key={index}
                   src={image}
                   alt=""
-                  className="absolute z-20 shadow-2xl  rounded-[20px]"
+                  className="absolute z-50  scale-[1.5]  rounded-[20px]"
                   style={{
                     display: index === visibleIndex ? "block" : "none",
                     height: "100%",
                     width: "100%",
-                    objectFit: "cover",
+                    objectFit: "contain",
                   }}
                 />
               ))}
             </div>
           </div>
           <div>
-            <ol className="w-full mt-10 lg:mt-32 space-y-20 list-none">
+            <ol className="w-full mt-10 z-0 lg:mt-32 space-y-40 list-none">
               {[
                 {
                   title: "Website Development",
                   description:
-                    "Crafting responsive and dynamic websites tailored to your specific needs. From simple landing pages to complex web applications, we bring your online presence to life.",
-                  points: [
-                    "Customized website development",
-                    "Responsive design for all devices",
-                    // "User-focused approach",y
-                  ],
+                    "Crafting responsive and dynamic websites tailored to your specific needs.",
                 },
                 {
                   title: "UI/UX Designing",
                   description:
-                    "Creating visually appealing and user-friendly interfaces that enhance the overall user experience. Our designs focus on usability, accessibility, and aesthetics.",
-                  points: [
-                    "Intuitive user interface design",
-                    "User experience optimization",
-                    // "Aesthetic and modern design principles",
-                  ],
+                    "Creating visually appealing and user-friendly interfaces that enhance the overall user experience.",
                 },
                 {
                   title: "Content Writing",
                   description:
-                    "Delivering compelling and engaging content for your website, blog, or marketing materials. We specialize in creating content that resonates with your target audience.",
-                  points: [
-                    "Strategic content creation",
-                    "Target audience alignment",
-                    // "SEO-optimized writing",
-                  ],
+                    "Delivering compelling and engaging content for your website, blog, or marketing materials. ",
                 },
                 {
                   title: "Graphic Designing",
                   description:
-                    "Transforming ideas into visually stunning graphics. Our graphic design services cover everything from logos and branding to promotional materials and print design. We focus on bringing your concepts to life through creative and impactful visual elements.",
-                  points: [
-                    "Logo and branding design",
-                    "Promotional materials creation",
-                    // "Print design for various media",
-                  ],
+                    "Transforming ideas into visually stunning graphics. ",
                 },
                 {
                   title: "Video Editing",
                   description:
-                    "Editing and enhancing video content to tell your story effectively. From promotional videos to tutorials, we ensure your message is communicated seamlessly through professional video editing.",
-                  points: [
-                    "Professional video editing services",
-                    "Effective storytelling through visuals",
-                    // "Promotional and tutorial video creation",
-                  ],
+                    "Editing and enhancing video content to tell your story effectively.",
                 },
               ].map((item, index) => (
                 <li
                   key={index}
-                  className="service-list-item  z-0 font-semibold text-[20px] mb-8 relative"
+                  className="service-list-item z-0 font-semibold text-[20px] mb-8 relative"
                 >
                   {/* <span className="bullet-point absolute">&#8226;</span> */}
                   <div>
-                    <span className="block ">
-                      {index + 1}. {item.title}
+                    <span className="block headings text-[40px]">
+                      {item.title}
                     </span>
-                    <ul className="list-none font-normal backdrop-blur-3xl  headings text-base ml-6">
+                    <ul className="list-none font-normal text-[20px] mt-5">
                       <li>{item.description}</li>
-                      {item.points && (
-                        <ul className="list-disc font-normal  text-base ml-6">
-                          {item.points.map((point, pointIndex) => (
-                            <li key={pointIndex}>{point}</li>
-                          ))}
-                        </ul>
-                      )}
                     </ul>
                   </div>
                 </li>
