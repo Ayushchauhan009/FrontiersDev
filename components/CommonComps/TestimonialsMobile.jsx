ff26efe082c7af343847e556dfd7d213a55dac82
aayushchauhan10 <aayushchauhanrajput47@gmail.com>
2024-04-20 13:43:14 +0530

    Completed frontend of contact section - desktop and mobile

--- components/CommonComps/TestimonialsMobile.jsx
+++ components/CommonComps/TestimonialsMobile.jsx
@@ -1,19 +1,13 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-cube";
 import "swiper/css/pagination";
-
 import { EffectCube, Pagination } from "swiper/modules";
-
-import "./styleMob.css";
 
 const TestimonialsMobile = () => {
   return (
-    <div>
+    <div className="w-full h-[400px]">
       <Swiper
         effect={"cube"}
         grabCursor={true}
@@ -27,41 +21,49 @@ const TestimonialsMobile = () => {
         modules={[EffectCube, Pagination]}
         className="mySwiper swiper2"
       >
-        <SwiperSlide className="swiper-slide2 font-space text-center py-4 lg:py-0 px-4">
-          Working with FrontiersDev has transformed our travel business. Their
-          stunning website and SEO strategies boosted our online visibility.
-          FrontiersDev's work has been a game-changer. Their website and SEO
-          strategies elevated our business.
-          <br />
-          <br />
-          ~Apoorv Joshi
+        <SwiperSlide className="py-10 bg-center bg-transparent bg-cover">
+          <p className="font-space text-center py-4 lg:py-0 px-4">
+            Working with FrontiersDev has transformed our travel business. Their
+            stunning website and SEO strategies boosted our online visibility.
+            FrontiersDev's work has been a game-changer. Their website and SEO
+            strategies elevated our business.
+            <br />
+            <br />
+            ~Apoorv Joshi
+          </p>
         </SwiperSlide>
-        <SwiperSlide className="swiper-slide2 font-space text-center py-4 lg:py-0 px-4">
-          Choosing FrontiersDev for our e-commerce site was one of our best
-          decisions. Their API integration and payment gateway setup streamlined
-          operations. FrontiersDev's e-commerce development was a game-changer
-          for us.
-          <br />
-          <br />
-          ~Priyanka Tonk
+        <SwiperSlide className="py-10 bg-center bg-cover">
+          <p className="font-space text-center py-4 lg:py-0 px-4">
+            Choosing FrontiersDev for our e-commerce site was one of our best
+            decisions. Their API integration and payment gateway setup
+            streamlined operations. FrontiersDev's e-commerce development was a
+            game-changer for us.
+            <br />
+            <br />
+            ~Priyanka Tonk
+          </p>
         </SwiperSlide>
-        <SwiperSlide className="swiper-slide2 font-space text-center py-4 lg:py-0 px-4">
-          Partnering with FrontiersDev on my YouTube journey has been
-          incredible. Their support in creating my recipe website was
-          invaluable. FrontiersDev understood and brought to life my vision for
-          a recipe app.
-          <br />
-          <br />
-          ~Megha Chauhan
+        <SwiperSlide className="py-10 bg-center bg-cover">
+          <p className="font-space text-center py-4 lg:py-0 px-4">
+            Partnering with FrontiersDev on my YouTube journey has been
+            incredible. Their support in creating my recipe website was
+            invaluable. FrontiersDev understood and brought to life my vision
+            for a recipe app.
+            <br />
+            <br />
+            ~Megha Chauhan
+          </p>
         </SwiperSlide>
-        <SwiperSlide className="swiper-slide2 font-space text-center py-4 lg:py-0 px-4">
-          Collaborating with FrontiersDev on my hotel booking site was
-          fantastic. They used NextJs technology and stellar SEO to bring my
-          vision to life. FrontiersDev's attention to detail in design and
-          content creation set them apart.
-          <br />
-          <br />
-          ~Sonu Sharma
+        <SwiperSlide className="py-10 bg-center bg-cover">
+          <p className="font-space text-center py-4 lg:py-0 px-4">
+            Collaborating with FrontiersDev on my hotel booking site was
+            fantastic. They used NextJs technology and stellar SEO to bring my
+            vision to life. FrontiersDev's attention to detail in design and
+            content creation set them apart.
+            <br />
+            <br />
+            ~Sonu Sharma
+          </p>
         </SwiperSlide>
       </Swiper>
     </div>
