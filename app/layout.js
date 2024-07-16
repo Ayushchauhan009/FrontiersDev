8cd17179add78581a7d660722b19997ffce7de49
Ayush_Chauhan <aayushchauhanrajput47@gmail.com>
2024-07-16 13:26:21 +0530

    Revert "Adding SEO OG"
    This reverts commit bd5ac51a27f7520b39bff2b2e43117370792283f.

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <Head>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-LVWQNBY8Y4"></script>
         <script>
